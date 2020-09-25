dccb9537300cd9ef7931f6f9209215a5031391c4
Kaldzkur <kaldzkur@gmail.com>
2020-09-25 20:35:22 +0000

    Updated shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 if [ -z "$1" ] ; then
+  echo "No Args"
   #No input argument passed -> kill all base_bot instances
-  sudo pkill -f base_bot.py
+  sudo pkill -f cyra.py
   #wait for all processes to end
-  PID=$(/usr/bin/pgrep -f base_bot.py)
+  PID=$(/usr/bin/pgrep -f cyra.py)
   while [ "$PID" != "" ];do
     sleep 1
-    sudo pkill -f base_bot.py
-    PID=$(/usr/bin/pgrep -f base_bot.py)
+    sudo pkill -f cyra.py
+    PID=$(/usr/bin/pgrep -f cyra.py)
   done
 else
   #kill only the process with the given pid (arg 1 passed to script)
   sudo kill $1
   #wait for all processes to end
-  PID=$(/usr/bin/pgrep -f base_bot.py)
+  PID=$(/usr/bin/pgrep -f cyra.py)
   while [ "$PID" != "" ];do
     sleep 1
     sudo kill $1
-    PID=$(/usr/bin/pgrep -f base_bot.py)
+    PID=$(/usr/bin/pgrep -f cyra.py)
   done
 fi
