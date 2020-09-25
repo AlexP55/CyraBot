8d264784d8d4e1e38490cca8f07c6909ac2fd77f
alexP55 <AlxPn9696@gmail.com>
2020-09-25 15:28:03 -0700

    signal force kill

--- shutdown.sh
+++ shutdown.sh
@@ -2,22 +2,15 @@
 if [ -z "$1" ] ; then
   echo "No Args"
   #No input argument passed -> kill all base_bot instances
-  sudo pkill -f cyra.py
+  sudo pkill -9 -f cyra.py
   #wait for all processes to end
   PID=$(/usr/bin/pgrep -f cyra.py)
   while [ "$PID" != "" ];do
     sleep 1
-    sudo pkill -f cyra.py
+    sudo pkill -9 -f cyra.py
     PID=$(/usr/bin/pgrep -f cyra.py)
   done
 else
   #kill only the process with the given pid (arg 1 passed to script)
-  sudo kill $1
-  #wait for all processes to end
-  PID=$(/usr/bin/pgrep -f cyra.py)
-  while [ "$PID" != "" ];do
-    sleep 1
-    sudo kill $1
-    PID=$(/usr/bin/pgrep -f cyra.py)
-  done
+  sudo kill -9 $1
 fi
