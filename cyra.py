1e3fc2665db512f22e68f2b1149dc2c955dfe927
alexP55 <AlxPn9696@gmail.com>
2021-02-07 18:50:30 -0600

    improve achieve command error message

--- cyra.py
+++ cyra.py
@@ -75,7 +75,7 @@ class CyraBot(BaseBot):
         else:
           msg = state["noability_msg_other"]
       elif isinstance(error, custom_exceptions.DataNotFound):
-        msg = state["nodata_msg"] + ("" if )
+        msg = state["nodata_msg"]
         if error.category.lower() == "achievement":
           msg = f"{msg}\nIf you are looking for other missions, please refer to https://realm-defense-hero-legends-td.fandom.com/wiki/Mission_Board"
       elif isinstance(error, commands.UserInputError):
