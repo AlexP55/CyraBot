1efdeb463eb39b7d3cae4a0845f471ab7ba9e4fa
alexP55 <AlxPn9696@gmail.com>
2021-03-08 21:17:49 -0800

    update leaderboard format, return ASCII strings

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -294,7 +294,7 @@ class LeaderboardCog(commands.Cog, name="Leaderboard Commands"):
         flag = "🏁"
       heroes = "".join([hero if hero is not None else "⬛" for hero in [hero1, hero2, hero3]])
       mention = member.name if member is not None else ""
-      table.append([str(rank), flag, name, heroes, str(gm_num), mention])
+      table.append([str(rank) if rank < 100 else "", flag, name, heroes, str(gm_num), mention])
     rank_maxlen = max([len(table[i][0]) for i in range(len(table))])
     name_maxlen = max([len(table[i][2]) for i in range(len(table))])
     gm_num_maxlen = max([len(table[i][4]) for i in range(len(table))])
@@ -352,7 +352,7 @@ class LeaderboardCog(commands.Cog, name="Leaderboard Commands"):
         minutes, seconds = divmod(time, 60)
         time = f"{minutes:02.0f}:{seconds:06.3f}"
       kill = "???" if kill is None else str(kill)
-      table.append([str(rank), flag, name, heroes, kill, time, mention])
+      table.append([str(rank) if rank < 100 else "", flag, name, heroes, kill, time, mention])
     rank_maxlen = max([len(table[i][0]) for i in range(len(table))])
     name_maxlen = max([len(table[i][2]) for i in range(len(table))])
     kill_maxlen = max([len(table[i][4]) for i in range(len(table))])
